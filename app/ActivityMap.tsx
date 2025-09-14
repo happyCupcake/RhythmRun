8a211317f7220226b9e30eb0d65773d62a531c3e
Mrinali <happymrinali@gmail.com>
2025-09-14 10:55:02 -0400

    map marker img improved

--- app/ActivityMap.tsx
+++ app/ActivityMap.tsx
@@ -42,9 +42,10 @@ export default function ActivityMap({ encodedPolyline, intervals = [] }: Activit
       style={{ height: "400px", width: "100%", borderRadius: "1rem" }}
     >
     <TileLayer
-    url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-    attribution="&copy; <a href='https://stadiamaps.com/'>Stadia Maps</a>, © <a href='https://openmaptiles.org/'>OpenMapTiles</a> © <a href='https://openstreetmap.org'>OpenStreetMap</a> contributors"
+    url="/tile.png"
+    attribution="&copy; Local Map Data"
     />
+
 
       <Polyline positions={positions} color="red" weight={4} />
 
