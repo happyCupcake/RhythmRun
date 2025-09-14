e2f42743237a3490852c845c3d2d9da9c3572bda
Nikita Ostapenko <nikitul2005@gmail.com>
2025-09-14 11:05:13 -0400

    change map

--- app/ActivityMap.tsx
+++ app/ActivityMap.tsx
@@ -54,7 +54,7 @@ export default function ActivityMap({ encodedPolyline, intervals = [] }: Activit
     <MapContainer
       center={positions[0]}
       zoom={13}
-      style={{ height: "400px", width: "100%", borderRadius: "1rem" }}
+      style={{ height: "400px", width: "80%", borderRadius: "1rem"  }}
     >
       <TileLayer
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
